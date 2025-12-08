0f6afb084214cae836daafa323ae82d934acaaa6
skokovin <s.kokovin@gmail.com>
2025-12-08 12:32:36 +0300

    2

--- build.rs
+++ build.rs
@@ -4,6 +4,10 @@ fn main() -> Result<()> {
     // В библиотеке build.rs делает то же самое: компилирует .proto
     // Важно: файл trade.proto теперь должен лежать внутри папки библиотеки: proto_types/proto/trade.proto
     println!("cargo:rerun-if-changed=proto/trade.proto");
-    prost_build::compile_protos(&["proto/trade.proto","proto/geyser.proto"], &["proto/"])?;
+    //prost_build::compile_protos(&["proto/trade.proto","proto/geyser.proto"], &["proto/"])?;
+    //tonic_build::compile_protos(&["proto/trade.proto","proto/geyser.proto"], &["proto/"])?;
+    tonic_prost_build::compile_protos("proto/trade.proto");
+    tonic_prost_build::compile_protos("proto/geyser.proto");
+
     Ok(())
 }
