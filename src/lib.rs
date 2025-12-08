daf84f6f2e53285cd3f3507de196fa3c81a6d007
skokovin <s.kokovin@gmail.com>
2025-12-08 12:15:37 +0300

    1

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,12 @@ pub mod my_exchange {
     include!(concat!(env!("OUT_DIR"), "/my_exchange.rs"));
 }
 
+pub mod geyser {
+    // Магия та же: мы включаем файл из скрытой папки OUT_DIR
+    include!(concat!(env!("OUT_DIR"), "/geyser.rs"));
+}
+
 // Опционально: можно сделать ре-экспорт, чтобы пользователи писали
 // use proto_types::Trade; вместо use proto_types::my_exchange::Trade;
 pub use my_exchange::*;
+pub use geyser::*;
