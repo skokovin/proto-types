f81de1e74d0a956e1d31cbdb1bd5e693ad836745
skokovin <s.kokovin@gmail.com>
2025-12-17 16:11:32 +0300

    Radar

--- build.rs
+++ build.rs
@@ -2,18 +2,30 @@ use std::env;
 use std::io::Result;
 
 fn main() -> Result<()> {
-    
+
+    println!("cargo:rerun-if-changed=proto");
+    tonic_prost_build::compile_protos("proto/geyser.proto");
+    tonic_prost_build::compile_protos("proto/radar.proto");
+
+    Ok(())
+}
+
+/*use std::env;
+use std::io::Result;
+
+fn main() -> Result<()> {
+
     println!("cargo:rerun-if-changed=proto");
 
-  
+
     if env::var("CARGO_FEATURE_INGEST").is_ok() {
         tonic_prost_build::compile_protos("proto/geyser.proto");
     }
 
-    
+
     if env::var("CARGO_FEATURE_RADAR").is_ok() {
         tonic_prost_build::compile_protos("proto/radar.proto");
     }
 
     Ok(())
-}
+}*/
